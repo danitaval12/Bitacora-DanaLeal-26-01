36411bc04bd1411cb70c9ee2189f6511ef509c22
danitaval12 <dana.leal-g@mail.escuelaing.edu.co>
2026-02-12 14:02:12 -0500

    Se agrega el Maven a nuestra bitacora

---
+++ src/test/java/edu/dosw/bitacora/Bitacora/Dana/Leal/BitacoraDanaLealApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.bitacora.Bitacora.Dana.Leal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitacoraDanaLealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
